20dc01e935f44be85e813cfe9269c79576aa184a
axatrikx <axatrikx@gmail.com>
2013-01-12 17:05:22 +0530

    Added game class structure;

---
+++ engine/game.cpp
@@ -0,0 +1,14 @@
+/*
+ * game.cpp
+ *
+ *  Created on: Jan 12, 2013
+ *      Author: amalbose
+ */
+
+#include "game.h"
+
+game::game() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ engine/game.h
@@ -0,0 +1,16 @@
+/*
+ * game.h
+ *
+ *  Created on: Jan 12, 2013
+ *      Author: amalbose
+ */
+
+#ifndef GAME_H_
+#define GAME_H_
+
+class game {
+public:
+	game();
+};
+
+#endif /* GAME_H_ */
