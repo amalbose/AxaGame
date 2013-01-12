90e6d9b690ad0dec06a49892367a15332e732aea
axatrikx <axatrikx@gmail.com>
2013-01-12 16:57:01 +0530

    Added contants header

---
+++ engine/constants.h
@@ -0,0 +1,15 @@
+/*
+ * constants.h
+ *
+ *  Created on: Jan 12, 2013
+ *      Author: amalbose
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+const char * const GAME_VERSION = "v0.1.1";
+
+
+
+#endif /* CONSTANTS_H_ */
